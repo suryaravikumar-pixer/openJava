6bf5e175d885cf404759e1629cb18817e9a1af40
suryaravikumar-pixer <suryaravikumar5@gmail.com>
2022-06-17 07:39:18 +0530

    first commit

---
+++ openJava/openJava/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+            Joy();
+            Joy();
+            Joy();
+    }
+    static void Joy(){
+        System.out.println("Joy lines in japan");
+    }
+}
---
+++ openJava/openJava/src/openjava/core/variables/VariableClassOfJava.java
@@ -0,0 +1,54 @@
+package openjava.core.variables;
+
+public class VariableClassOfJava {
+    public static void main(String[] args) {
+
+
+        // number related
+        int APGVB = 2147483647;
+         long Swiss = 9223372036854775807L;
+
+        float marks = 2147483647f;
+        double usB = 9223372036854775807d;
+
+        boolean doYouWantToBeSomething = false;
+        char ch1 = '1';
+//        char ch2 = 'ayu';
+        char ch3 = '=';
+
+        // defalut values of variables
+        String  ch5 = null;
+        int height = 0;
+        boolean bool = false;
+
+        byte bite =  -111;
+
+        System.out.println(ch1);
+        System.out.println(ch2);
+        System.out.println(ch5);
+        System.out.println(ch3);
+
+
+        byte bitByte = (byte) 712345118;
+        System.out.println(bitByte);
+        System.out.println(APGVB);
+        System.out.println("Swiss bank  "+ Swiss); // concatination  this is single line comment
+        /** *
+         * what is variable declaration & assigning?
+         *
+         *
+         * **/
+
+        String practiceProgramming;  // declaration
+        practiceProgramming = "yes , i'm practicing programming daily";  // assigning
+
+        System.out.println(practiceProgramming);
+
+
+
+
+
+    }
+
+
+}
