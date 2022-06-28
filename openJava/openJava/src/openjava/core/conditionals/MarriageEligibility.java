e0293c0ea79cac1028e1a370e38d8e452a55143f
suryaravikumar-pixer <suryaravikumar5@gmail.com>
2022-06-28 07:29:31 +0530

    conditionals and switch are added on 28062022

---
+++ openJava/openJava/src/openjava/core/conditionals/MarriageEligibility.java
@@ -0,0 +1,47 @@
+package openjava.core.conditionals;
+
+public class MarriageEligibility {
+    public static void main(String[] args) {
+        // just for function calling
+        int age = 0;
+        String country = "USA";
+        checkEligibility(country, age);
+    }
+
+
+    static void checkEligibility(String country, int age) {
+        // logic building && check base conditions
+        if(age == 0){
+         System.out.println("enter natural number or enter 1 onwards 2,3,4,");
+         }
+
+        if (country == null){
+         System.out.println("enter usa or india");
+         }
+
+
+      try {
+          switch (country.toLowerCase()) {
+              case "india":
+                  if (age >= 21) {
+                      System.out.println("eligible in india");
+                  } else {
+                      System.out.println("not eligible india");
+                  }
+                  break;
+              case "usa":
+                  if (age < 18) {
+                      System.out.println("not eligible less than 18 in usa");
+                  } else if (age >= 18 && age < 21) {
+                      System.out.println("eligible between 18 and 21 ");
+                  } else {
+                      System.out.println(" eligible above 21 in usa");
+                  }
+                  break;
+          }
+      }
+      catch (NullPointerException induraj){
+          System.out.println(induraj);
+      }
+    }
+}
