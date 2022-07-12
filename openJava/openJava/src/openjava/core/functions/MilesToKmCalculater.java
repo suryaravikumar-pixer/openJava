a1156361cfe63c7c3a35e809afd7f29d24ffe6d9
suryaravikumar-pixer <suryaravikumar5@gmail.com>
2022-07-12 22:30:02 +0530

    changes in the method

--- openJava/openJava/src/openjava/core/functions/MilesToKmCalculater.java
+++ openJava/openJava/src/openjava/core/functions/MilesToKmCalculater.java
@@ -1,15 +1,19 @@
 package openjava.core.functions;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class MilesToKmCalculater {
-    public static void main(String[] args) {
-    double m;
-    Scanner sc = new Scanner(System.in);
 
-        System.out.println("enter no of miles :");
-        m = sc.nextDouble();
-        System.out.println(m + "miles = " +m*1.6+ "km");
-        }
+    public static void milesToKmCalculator(double miles) {
+        System.out.println(miles + "miles = " + miles * 1.6+ "km");
+
     }
+
+    public static void main(String[] args) {
+        double m;
+        System.out.println("enter no of miles :");
+        Scanner sc = new Scanner(System.in);
+        m = sc.nextDouble();
+        milesToKmCalculator(m);
+    }
+}
