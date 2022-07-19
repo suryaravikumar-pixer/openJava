7b11fc0ab078c51f27ca87d73a7400693dea64d9
suryaravikumar-pixer <suryaravikumar5@gmail.com>
2022-07-19 07:07:36 +0530

    Method overriding concept is added on 19072022

---
+++ openJava/openJava/src/openjava/core/overriding/overloading/RunMethodOverriding.java
@@ -0,0 +1,32 @@
+package openjava.core.overriding.overloading;
+
+public class RunMethodOverriding {
+    public static void main(String[] args) {
+        GrandParent grandParent = new Child();
+        grandParent.arithmetic(100,10);
+    }
+}
+
+class GrandParent{
+
+    void arithmetic(int value1, int value2){
+        int result = value1 / value2;
+        System.out.println("Grand parents devide the "+value1+"/"+value2+" ="+result);
+    }
+}
+
+class Parent extends GrandParent{
+    @Override
+    void arithmetic(int value1, int value2){
+        int result = value1 + value2;
+        System.out.println(" parents add the "+value1+"+"+value2+" ="+result);
+    }
+}
+
+class Child extends Parent{
+    @Override
+    void arithmetic(int value1, int value2){
+        int result = value1 * value2;
+        System.out.println("Child multipluy the wealth the "+value1+"*"+value2+" ="+result);
+    }
+}
